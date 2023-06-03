bcace4f72dab590078e60b880224a2eaced90483
KonYaco <atwzj233@gmail.com>
2023-06-04 01:54:37 +0800

    Update AGP version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.0.0-alpha05" apply false
-    id("com.android.library") version "8.0.0-alpha05" apply false
+    id("com.android.application") version "8.0.2" apply false
+    id("com.android.library") version "8.0.2" apply false
     kotlin("android") version "1.7.20" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
 }
