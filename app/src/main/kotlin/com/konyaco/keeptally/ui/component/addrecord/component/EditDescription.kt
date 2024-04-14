816c5bec6f1353e395213f80f29aacc704a1e14e
KonYaco <atwzj233@gmail.com>
2024-04-14 16:55:10 +0800

    Limit description to 1 line

--- app/src/main/kotlin/com/konyaco/keeptally/ui/component/addrecord/component/EditDescription.kt
+++ app/src/main/kotlin/com/konyaco/keeptally/ui/component/addrecord/component/EditDescription.kt
@@ -4,12 +4,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 
@@ -26,19 +28,18 @@ internal fun EditDescription(
                 .padding(vertical = 12.dp),
             value = value,
             onValueChange = onValueChange,
-            decorationBox = {
-                Box(
-                    Modifier
-                        .fillMaxWidth()
-                ) {
-                    it()
+            decorationBox = { content ->
+                Box(Modifier.fillMaxWidth()) {
+                    content()
                     if (value.text.isEmpty()) {
                         Text("添加备注", color = LocalContentColor.current.copy(0.7f))
                     }
                 }
             },
             textStyle = LocalTextStyle.current.copy(LocalContentColor.current),
-            cursorBrush = SolidColor(LocalContentColor.current)
+            cursorBrush = SolidColor(LocalContentColor.current),
+            maxLines = 1,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done)
         )
     }
 }
