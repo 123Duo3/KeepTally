fa882eb4f84f32762f670e23a2b1067095f6b1dc
KonYaco <atwzj233@gmail.com>
2024-12-05 03:19:33 +0800

    Move bottom sheet to DetailScreen

--- app/src/main/kotlin/com/konyaco/keeptally/ui/App.kt
+++ app/src/main/kotlin/com/konyaco/keeptally/ui/App.kt
@@ -1,66 +1,35 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.konyaco.keeptally.ui
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.End
 import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.Start
 import androidx.compose.animation.ContentTransform
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.selection.toggleable
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.SheetState
-import androidx.compose.material3.SheetValue
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.rememberUpdatedState
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.konyaco.keeptally.di.sharedViewModel
 import com.konyaco.keeptally.ui.component.HomeTopBar
 import com.konyaco.keeptally.ui.component.HomeTopBarState
-import com.konyaco.keeptally.ui.component.addrecord.AddRecord
 import com.konyaco.keeptally.ui.component.rememberHomeTopBarState
 import com.konyaco.keeptally.ui.detail.DetailScreen
 import com.konyaco.keeptally.ui.filter.FilterScreen
@@ -84,52 +53,15 @@ fun App(
             contentColor = MaterialTheme.colorScheme.onBackground
         ) {
             Box {
-                var openSheet by remember { mutableStateOf(false) }
-                val localFocus = rememberUpdatedState(LocalFocusManager.current)
-                val scope = rememberCoroutineScope()
-                val sheetState = rememberModalBottomSheetState(
-                    confirmValueChange = {
-                        if (it == SheetValue.Hidden)
-                            localFocus.value.clearFocus()
-                        true
-                    },
-                    skipPartiallyExpanded = true
-                )
-                CompositionLocalProvider(LocalSheetState provides sheetState) {
-                    Content(viewModel, onAddClick = {
-                        openSheet = true
-                    })
-                }
+                Content(viewModel)
+
                 SnackbarHost(
                     sharedViewModel.snackbarHostState,
                     Modifier
                         .align(Alignment.BottomCenter)
                         .padding(bottom = 32.dp)
                 )
-                if (openSheet) ModalBottomSheet(
-                    sheetState = sheetState,
-                    shape = RectangleShape,
-                    onDismissRequest = {
-                        openSheet = false
-                    },
-                    containerColor = MaterialTheme.colorScheme.surface
-                ) {
-                    AddRecord(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .imePadding(),
-                        onCloseRequest = {
-                            localFocus.value.clearFocus()
-                            scope.launch {
-                                sheetState.hide()
-                            }.invokeOnCompletion {
-                                if (!sheetState.isVisible) {
-                                    openSheet = false
-                                }
-                            }
-                        }
-                    )
-                }
+
             }
 
         }
@@ -137,8 +69,8 @@ fun App(
 }
 
 @Composable
-private fun Content(viewModel: MainViewModel, onAddClick: () -> Unit) {
-    ContentPager(viewModel, onAddClick)
+private fun Content(viewModel: MainViewModel) {
+    ContentPager(viewModel)
 }
 
 @Composable
@@ -179,7 +111,7 @@ private fun ContentAnimatedContent(viewModel: MainViewModel) {
             }, label = "content"
         ) {
             when (it) {
-                HomeTopBarState.TabItem.Detail -> DetailScreen(onAddClick = {})
+                HomeTopBarState.TabItem.Detail -> DetailScreen()
                 HomeTopBarState.TabItem.Filter -> FilterScreen()
                 HomeTopBarState.TabItem.Statistics -> StatisticScreen()
                 HomeTopBarState.TabItem.Other -> OtherScreen()
@@ -188,11 +120,9 @@ private fun ContentAnimatedContent(viewModel: MainViewModel) {
     }
 }
 
-
 @Composable
 private fun ContentPager(
-    viewModel: MainViewModel,
-    onAddClick: () -> Unit
+    viewModel: MainViewModel
 ) {
     Column(
         Modifier
@@ -256,99 +186,10 @@ private fun ContentPager(
             key = { it }
         ) {
             when (it) {
-                0 -> DetailScreen(onAddClick = onAddClick)
+                0 -> DetailScreen()
                 1 -> FilterScreen()
                 2 -> StatisticScreen()
                 3 -> OtherScreen()
-            }
-        }
-    }
-}
-
-val LocalSheetState = compositionLocalOf<SheetState> {
-    error("No ModalBottomSheetState provided")
-}
-
-@Composable
-@Preview
-private fun Test() {
-    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
-    var skipPartiallyExpanded by rememberSaveable { mutableStateOf(false) }
-    val scope = rememberCoroutineScope()
-    val bottomSheetState =
-        rememberModalBottomSheetState(skipPartiallyExpanded = skipPartiallyExpanded)
-
-    // App content
-    Column(
-        horizontalAlignment = Alignment.Start,
-        verticalArrangement = Arrangement.spacedBy(4.dp)
-    ) {
-        Row(
-            Modifier.toggleable(
-                value = skipPartiallyExpanded,
-                role = Role.Checkbox,
-                onValueChange = { checked -> skipPartiallyExpanded = checked }
-            )
-        ) {
-            Checkbox(checked = skipPartiallyExpanded, onCheckedChange = null)
-            Spacer(Modifier.width(16.dp))
-            Text("Skip partially expanded State")
-        }
-        Button(
-            onClick = { openBottomSheet = !openBottomSheet },
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        ) {
-            Text(text = "Show Bottom Sheet")
-        }
-    }
-
-    // Sheet content
-    if (openBottomSheet) {
-
-        ModalBottomSheet(
-            onDismissRequest = { openBottomSheet = false },
-            sheetState = bottomSheetState,
-        ) {
-            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-                Button(
-                    // Note: If you provide logic outside of onDismissRequest to remove the sheet,
-                    // you must additionally handle intended state cleanup, if any.
-                    onClick = {
-                        scope
-                            .launch { bottomSheetState.hide() }
-                            .invokeOnCompletion {
-                                if (!bottomSheetState.isVisible) {
-                                    openBottomSheet = false
-                                }
-                            }
-                    }
-                ) {
-                    Text("Hide Bottom Sheet")
-                }
-            }
-            var text by remember { mutableStateOf("") }
-            OutlinedTextField(
-                value = text,
-                onValueChange = { text = it },
-                modifier = Modifier.padding(horizontal = 16.dp),
-                label = { Text("Text field") }
-            )
-            LazyColumn {
-                items(25) {
-                    ListItem(
-                        headlineContent = { Text("Item $it") },
-                        leadingContent = {
-                            Icon(
-                                Icons.Default.Favorite,
-                                contentDescription = "Localized description"
-                            )
-                        },
-                        colors =
-                        ListItemDefaults.colors(
-                            containerColor = MaterialTheme.colorScheme.surfaceContainerLow
-                        ),
-                    )
-                }
             }
         }
     }
--- app/src/main/kotlin/com/konyaco/keeptally/ui/detail/DetailScreen.kt
+++ app/src/main/kotlin/com/konyaco/keeptally/ui/detail/DetailScreen.kt
@@ -12,28 +12,41 @@ import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastForEachIndexed
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.konyaco.keeptally.R
+import com.konyaco.keeptally.ui.component.addrecord.AddRecord
 import com.konyaco.keeptally.ui.detail.component.AddRecordButton
 import com.konyaco.keeptally.ui.detail.component.DailyRecord
 import com.konyaco.keeptally.ui.detail.component.LineChart
@@ -43,10 +56,25 @@ import com.konyaco.keeptally.viewmodel.MainViewModel
 import com.konyaco.keeptally.viewmodel.MainViewModel.Companion.State.Done
 import com.konyaco.keeptally.viewmodel.MainViewModel.Companion.State.Initializing
 import com.konyaco.keeptally.viewmodel.MainViewModel.Companion.State.Loading
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailScreen(viewModel: MainViewModel = hiltViewModel(), onAddClick: () -> Unit) {
+fun DetailScreen(viewModel: MainViewModel = hiltViewModel()) {
     Box(Modifier.fillMaxSize()) {
+        var openSheet by remember { mutableStateOf(false) }
+        val localFocus = rememberUpdatedState(LocalFocusManager.current)
+
+        val scope = rememberCoroutineScope()
+        val sheetState = rememberModalBottomSheetState(
+            confirmValueChange = {
+                if (it == SheetValue.Hidden)
+                    localFocus.value.clearFocus()
+                true
+            },
+            skipPartiallyExpanded = true
+        )
+
         when (viewModel.state.value) {
             Initializing, Loading -> CircularProgressIndicator(Modifier.align(Alignment.Center))
             Done -> {
@@ -80,11 +108,35 @@ fun DetailScreen(viewModel: MainViewModel = hiltViewModel(), onAddClick: () -> U
                 AddRecordButton(
                     modifier = Modifier.align(Alignment.BottomEnd),
                     onAddClick = {
-                        onAddClick()
-//                        scope.launch { sheetState.show() }
+                        openSheet = true
                     }
                 )
             }
+        }
+
+        if (openSheet) ModalBottomSheet(
+            sheetState = sheetState,
+            shape = RectangleShape,
+            onDismissRequest = {
+                openSheet = false
+            },
+            containerColor = MaterialTheme.colorScheme.surface
+        ) {
+            AddRecord(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .imePadding(),
+                onCloseRequest = {
+                    localFocus.value.clearFocus()
+                    scope.launch {
+                        sheetState.hide()
+                    }.invokeOnCompletion {
+                        if (!sheetState.isVisible) {
+                            openSheet = false
+                        }
+                    }
+                }
+            )
         }
     }
 }
